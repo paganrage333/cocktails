ec31fc29e29d0350be8f83ec98ff715d2a6e52ce
paganrage333 <aragan@unca.edu>
2023-10-10 09:54:42 -0400

    started writing routes, models, forms

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask, render_template, redirect, session, request, g, flash
+from models import connect_db, db, get_cocktail_data, User
+from sqlalchemy.exc import IntegrityError
+from forms import UserAddForm, UserEditForm, LoginForm
+
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cocktails'
+app.config['SECRET_KEY'] = 'colada'
+app.app_context().push()
+
+connect_db(app)
+db.create_all()
+
+############# SIGN UP / LOGIN / LOGOUT ROUTES ############
+
+@app.before_request
+def add_user_to_g():
+    """If logged in, add curr user to Flask global"""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If the there already is a user with that username: flash message
+    and re-present form.
+    """
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+    form = UserAddForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                username=form.username.data,
+                password=form.password.data,
+                email=form.email.data,
+                image_url=form.image_url.data or User.image_url.default.arg,
+            )
+            db.session.commit()
+
+        except IntegrityError:
+            flash("Username already taken", 'danger')
+            return render_template('users/signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('users/signup.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('users/login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+
+    do_logout()
+
+    flash("You have successfully logged out.", 'success')
+    return redirect("/login")
+
+
---
+++ models.py
@@ -0,0 +1,151 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask import requests
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+def get_cocktail_data(flavor_pref, liquor_pref, diet):
+    base_url = "https://www.thecocktaildb.com/api.php"
+
+    params = {
+        "i": liquor_pref,
+        "c": flavor_pref,
+        "a": diet
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        return data
+    else:
+        return None 
+    
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    image_url = db.Column(
+        db.Text,
+        default="/static/images/default-pic.png",
+    )
+
+    header_image_url = db.Column(
+        db.Text,
+        default="/static/images/warbler-hero.jpg"
+    )
+
+    bio = db.Column(
+        db.Text,
+    )
+
+    location = db.Column(
+        db.Text,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    # messages = db.relationship('Message')
+
+    # followers = db.relationship(
+    #     "User",
+    #     secondary="follows",
+    #     primaryjoin=(Follows.user_being_followed_id == id),
+    #     secondaryjoin=(Follows.user_following_id == id)
+    # )
+
+    # following = db.relationship(
+    #     "User",
+    #     secondary="follows",
+    #     primaryjoin=(Follows.user_following_id == id),
+    #     secondaryjoin=(Follows.user_being_followed_id == id)
+    
+
+    likes = db.relationship(
+        'Message',
+        secondary="likes"
+    )
+
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}, {self.email}>"
+
+    # def is_followed_by(self, other_user):
+    #     """Is this user followed by `other_user`?"""
+
+    #     found_user_list = [user for user in self.followers if user == other_user]
+    #     return len(found_user_list) == 1
+
+    # def is_following(self, other_user):
+    #     """Is this user following `other_use`?"""
+
+    #     found_user_list = [user for user in self.following if user == other_user]
+    #     return len(found_user_list) == 1
+
+    @classmethod
+    def signup(cls, username, email, password, image_url):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            image_url=image_url,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+    
+    
